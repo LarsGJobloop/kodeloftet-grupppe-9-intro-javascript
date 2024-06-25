a175e8a376858eea24fae9ebbf8c558735f25610
LarsGJobloop <lars.gunnar@jobloop.no>
2024-06-25 11:02:02 +0200

    add examples for the primitive data types

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
-console.log('Hello!');
+// "let" er ett nøkkelord for å lage en ny variabel
+// "greeting" er et vavlgfritt navn
+// "=" lagre verdien på høyre side i navnet på venstre
+// 'God Morgen Kodeloftet' er verdien som blir lagret
+let greeting = 'God Morgen Kodeloftet'
+
+// Skriv innholdet i "greeting" variablen til konsollen
+console.log(greeting)
+
+let numberA = 10
+let numberB = 32
+
+console.log("Tall A:")
+console.log(numberA)
+
+console.log("Tall B:")
+console.log(numberB)
+
+let numberC = numberA / numberB + numberA - numberB
+
+console.log("Tall C:")
+console.log(numberC)
+
+// Grunnleggend Data Typer
+
+// Stringer (tekst)
+let stringA = "Hello"
+let stringB = 'World'
+let stringC = `!!!`
+
+let stringD = " Lets 'Go'!"
+console.log(stringD)
+
+let newLine = "Hello\n\n\nWorld"
+console.log(newLine)
+let multiLine = `
+Testing
+testing
+testing
+`
+console.log(multiLine)
+
+// Number
+let a = 10
+
+// Boolean
+let boolTrue = true
+let boolFalse = false
+
+let isPasswordValid = false
+
+let x = 10
+let y = 20
+let isEqual = x === y
+console.log("is the numbers equal")
+console.log(isEqual)
+
+let nameA = "Lars"
+let nameB = "Tanja"
+let isNamesEqual = nameA === nameB
+console.log("is the names equal")
+console.log(isNamesEqual)
+
+// Udefinert
+let thisIsUndefined
+console.log("This is undefined")
+console.log(thisIsUndefined)
+
+// Null
+let doesNotYetHaveValue = null
+console.log("The following does not yet have a value:")
+console.log(doesNotYetHaveValue)
